3fbdda32443ea8e2c67256449eafe69263556683
kuronight29 <herryvu.dev@gmail.com>
2024-09-22 20:14:44 +0700

    uuup

---
+++ Learning Map/SCORES/main.cpp
@@ -0,0 +1,16 @@
+/*
+    @author longvuuuu
+    @github kuronight29
+*/
+#include <bits/stdc++.h>
+#define taskname ""
+#define ll long long
+#define pb push_back
+using namespace std;
+int main() {
+    //freopen(taskname ".inp", "r", stdin);
+    //freopen(taskname ".out", "w", stdout);
+    map<ll,pair<string,ll>> m;
+    
+    return 0;
+}
