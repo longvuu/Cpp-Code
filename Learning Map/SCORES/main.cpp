9bdab96cb38ffb2ff4a19367b24a0c12e4e5d8f0
kuronight29 <herryvu.dev@gmail.com>
2024-09-22 21:10:07 +0700

    up

--- Learning Map/SCORES/main.cpp
+++ Learning Map/SCORES/main.cpp
@@ -8,9 +8,27 @@
 #define pb push_back
 using namespace std;
 int main() {
-    //freopen(taskname ".inp", "r", stdin);
-    //freopen(taskname ".out", "w", stdout);
-    map<ll,pair<string,ll>> m;
-    
+    freopen(taskname ".inp", "r", stdin);
+    freopen(taskname ".out", "w", stdout);
+    map<string,ll> m;
+    ll n;
+    cin >> n;
+    while(n--){
+        ll s=0;
+        string y;
+        ll x,z;
+        cin >> x >> y;
+        if(x==1){
+            cin >> z;
+            s=0;
+            s=m[y];
+            s+=z;
+            m[y]=s;
+        }else if(x==2){
+            m[y]=0;
+        }else if(x==3){
+            cout << m[y]<<endl;
+        }
+    }
     return 0;
 }
