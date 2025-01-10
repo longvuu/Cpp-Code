3b26592a42372da7f56c421ca1a8386be1230ec9
Vũ Đức Long <herryvu.dev@gmail.com>
2025-01-10 20:15:23 +0700

    Update DSB.cpp

--- yendinh1/DSB.cpp
+++ yendinh1/DSB.cpp
@@ -10,12 +10,13 @@
 #define simp main
 #define pb push_back
 using namespace std;
-ll n,res=0;
+ll res=0;
 int simp() {
     if(fopen((string(taskname) + ".inp").c_str(), "r") != NULL) {
         freopen((string(taskname) + ".inp").c_str(), "r", stdin);
         freopen((string(taskname) + ".out").c_str(), "w", stdout);
     }
+    ll n;
     cin >> n;
     vector<ll> a(n+1);
     vector<ll> s(5,0);
@@ -24,18 +25,14 @@ int simp() {
         s[a[i]]++;
     }
     res+=s[4];
-    if(s[2]%2==0){
-        res+=s[2]/2;
-    }else{
-        res+=s[2]/2+1;
+    res += s[3]; 
+    s[1] = max(0LL, s[1] - s[3]); 
+    res += s[2] / 2; 
+    if (s[2] % 2) {
+        res++; 
+        s[1] = max(0LL, s[1] - 2); 
     }
-    if(s[3]==s[1]){
-        res+=s[3];
-    }else{
-        res+=min(s[3],s[1]);
-        res+=max(s[3],s[1])-min(s[3],s[1]);
-    }
-    cout << res;
-
+    res += (s[1] + 3) / 4; 
+    cout << res << endl;
     return 0;
 }
