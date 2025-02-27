5c4628833eadc41514cecaa52ddbcdc6d59f280b
Vũ Đức Long <herryvu.dev@gmail.com>
2025-02-27 14:39:04 +0000

    up

--- yendinh1/2 con trỏ/contest/BAI5.cpp
+++ yendinh1/2 con trỏ/contest/BAI5.cpp
@@ -3,14 +3,13 @@
     @github kuronight29
 */
 #include <bits/stdc++.h>
-#define taskname ""
+#define taskname "BAI5"
 #define ll long long
 #define fi first
 #define se second
 #define simp main
 #define pb push_back
 using namespace std;
-const int N = 1E8+1;
 int simp() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
@@ -18,22 +17,23 @@ int simp() {
         freopen((string(taskname) + ".inp").c_str(), "r", stdin);
         freopen((string(taskname) + ".out").c_str(), "w", stdout);
     }
-    ll n,k;
+    
+    ll n, k;
     cin >> n >> k;
     vector<ll> a(n);
-    vector<ll> s(N,0);
-    for(ll i = 0; i < n; i++){
+    map<ll, ll> s;
+    for(ll i = 0; i < n; i++) {
         cin >> a[i];
     }
-    bool kt=false;
-    ll l=0,r=0,res=0,x;
-    for(ll r=0;r<n;r++){
+    ll l = 0, res = 0; 
+    ll x = 0; 
+    for(ll r = 0; r < n; r++) {
         s[a[r]]++;
-        x=max(x,s[a[r]]);
-        while(l<=r&&x>=k){
-            res+=n-r;
+        x = max(x, s[a[r]]);
+        while(l <= r && x >= k) {
+            res += n - r;
             s[a[l]]--;
-            if(s[a[l]]==x-1){
+            if(s[a[l]] == x - 1) {
                 x--;
             }
             l++;
