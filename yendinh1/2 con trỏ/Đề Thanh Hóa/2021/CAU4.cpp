478d3c2f088aefafb7248f7d8cb31328f25d8694
kuronight29 <herryvu.dev@gmail.com>
2025-03-03 20:25:01 +0700

    up

--- yendinh1/2 con trỏ/Đề Thanh Hóa/2021/CAU4.cpp
+++ yendinh1/2 con trỏ/Đề Thanh Hóa/2021/CAU4.cpp
@@ -3,13 +3,16 @@
     @github kuronight29
 */
 #include <bits/stdc++.h>
-#define taskname ""
+#define taskname "CAU4"
 #define ll long long
 #define fi first
 #define se second
 #define simp main
 #define pb push_back
 using namespace std;
+bool ta(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
 
 int simp() {
     ios_base::sync_with_stdio(0);
@@ -19,6 +22,18 @@ int simp() {
         freopen((string(taskname) + ".out").c_str(), "w", stdout);
     }
     
-    
+    string s;
+    cin >> s;
+    ll n = s.length();
+    ll d1 = 0, d2 = 0;
+    for(int i = 0; i < n; i++) {
+        if(ta(s[i])) {
+            d1++;
+        } else {
+            d2++;
+        }
+    }
+    ll result = d1 * d2;
+    cout << result;
     return 0;
 }
