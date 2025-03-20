09c248f6ccae4b6a1a1dcdd1890d56ededfb8f58
kuronight29 <herryvu.dev@gmail.com>
2025-03-20 07:21:30 +0700

    update

--- yendinh1/2 con trỏ/Đề Thanh Hóa/2021-2022/BAI2.cpp
+++ yendinh1/2 con trỏ/Đề Thanh Hóa/2021-2022/BAI2.cpp
@@ -11,50 +11,61 @@
 #define pb push_back
 using namespace std;
 
-bool check(const string& a, const string& b) {
-    size_t i = 0, j = 0;
-    while (i < a.length()) {
-        if (j >= b.length()) return false; 
-        if (a[i] == b[j]) {
+bool sosanh(const string &a, const string &b)
+{
+    int i = 0, j = 0;
+    while (i < a.length() && j < b.length())
+    {
+        if (a[i] == b[j])
+        {
             i++;
             j++;
-        } else if (i > 0 && a[i] == a[i-1]) {
-            i++;  
-        } else {
+        }
+        else if (i > 0 && a[i] == a[i - 1])
+        {
+            i++;
+        }
+        else
+        {
             return false;
         }
     }
-    
-    return j == b.length(); 
+    if (j < b.length())
+    {
+        return false;
+    }
+    while (i < a.length())
+    {
+        if (i == 0 || a[i] != a[i - 1])
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
 }
 
-int simp() {
+int simp()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    if(fopen((string(taskname) + ".inp").c_str(), "r") != NULL) {
+    cin.tie(0);
+    cout.tie(0);
+    if (fopen((string(taskname) + ".inp").c_str(), "r") != NULL)
+    {
         freopen((string(taskname) + ".inp").c_str(), "r", stdin);
         freopen((string(taskname) + ".out").c_str(), "w", stdout);
     }
-    
-    string line1, line2;
-    getline(cin, line1);
-    getline(cin, line2);
-    
-    vector<string> a, b;
-    stringstream ss1(line1);
-    string word;
-    while (ss1 >> word) {
-        a.push_back(word);
-    }
-    
-    stringstream ss2(line2);
-    while (ss2 >> word) {
-        b.push_back(word);
-    }
-    
-    ll kq = 0;
-    for (size_t i = 0; i < a.size() && i < b.size(); i++) {
-        if (a[i] == b[i] || check(a[i], b[i])) {
+
+    string a, b;
+    getline(cin, a);
+    getline(cin, b);
+    istringstream ss1(a), ss2(b);
+    string s1, s2;
+    int kq = 0;
+    while (ss1 >> s1 && ss2 >> s2)
+    {
+        if (sosanh(s1, s2))
+        {
             kq++;
         }
     }
