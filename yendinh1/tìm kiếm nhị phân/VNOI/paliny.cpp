bdbe7e1162dd5917e04a25c5a1876faa193cfe81
kuronight29 <herryvu.dev@gmail.com>
2025-04-01 19:57:35 +0700

    upup

--- yendinh1/tìm kiếm nhị phân/VNOI/paliny.cpp
+++ yendinh1/tìm kiếm nhị phân/VNOI/paliny.cpp
@@ -3,14 +3,19 @@
     @github kuronight29
 */
 #include <bits/stdc++.h>
-#define taskname "BAI3"
+#define taskname ""
 #define ll long long
 #define fi first
 #define se second
 #define simp main
 #define pb push_back
 using namespace std;
+ll tknp(ll l,ll r){
+    ll mid=(l+r)/2;
+    ll res=0;
+    
 
+}
 int simp() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
