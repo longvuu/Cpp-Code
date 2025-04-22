32be8084039c5f7a33dffacd59a0da18ae39103b
kuronight29 <herryvu.dev@gmail.com>
2025-04-22 07:10:37 +0700

    up

--- yendinh1/tim kiem nhi phan/ReimuOJ/440.cpp
+++ yendinh1/tim kiem nhi phan/ReimuOJ/440.cpp
@@ -1,9 +1,5 @@
 /*
     @author longvuuuu
-<<<<<<< HEAD
-    @problem https://marisaoj.com/problem/440
-=======
->>>>>>> fad832f90b4d45cd6e957ec218786d3bddf0a454
 */
 #include <bits/stdc++.h>
 #define taskname ""
@@ -12,24 +8,8 @@
 #define se second
 #define pb push_back
 using namespace std;
-<<<<<<< HEAD
-bool check(vector<ll> w,vector<ll> v, int k, double x) {
-    int n = w.size();
-    vector<pair<double, int>> a(n);
-    for (int i = 0; i < n; i++) {
-        a[i] = {v[i] - x * w[i], i};
-    }
-    sort(a.begin(), a.end(), greater<pair<double, int>>());
-    double t = 0;
-    for (int i = 0; i < k; i++) {
-        t += a[i].fi;
-    }
-    return t >= 0;
-}
-int main() {
-=======
 ll  n, k;
-vector<pair<ll , ll >> a; // (value, weight)
+vector<pair<ll , ll >> a; 
 bool check(double x) {
     vector<double> s(n);
     for (ll  i = 0; i < n; i++) {
@@ -46,26 +26,12 @@ bool check(double x) {
     return t >= 0;
 }
 int  main() {
->>>>>>> fad832f90b4d45cd6e957ec218786d3bddf0a454
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     if(fopen((string(taskname) + ".inp").c_str(), "r") != NULL) {
         freopen((string(taskname) + ".inp").c_str(), "r", stdin);
         freopen((string(taskname) + ".out").c_str(), "w", stdout);
     }
-<<<<<<< HEAD
-    ll n, k;
-    cin >> n >> k;
-    vector<ll> w(n), v(n);
-    for (ll i = 0; i < n; i++) {
-        cin >> w[i] >> v[i];
-    }
-    double l = 0, r = 1e9, res = 0;
-    for (int j = 0; j < 100; j++) {
-        double mid = (l + r) / 2;
-        if (check(w, v, k, mid)) {
-            res = mid;
-=======
     cin >> n >> k;
     a.resize(n);
     for (ll  i = 0; i < n; i++) {
@@ -75,16 +41,11 @@ int  main() {
     while (r - l > 1e-9) {
         double mid = (l + r) / 2;
         if (check(mid)) {
->>>>>>> fad832f90b4d45cd6e957ec218786d3bddf0a454
             l = mid;
         } else {
             r = mid;
         }
     }
-<<<<<<< HEAD
-    cout << fixed << setprecision(9) << res << '\n';
-=======
     cout << fixed << setprecision(8) << l << endl;
->>>>>>> fad832f90b4d45cd6e957ec218786d3bddf0a454
     return 0;
 }
