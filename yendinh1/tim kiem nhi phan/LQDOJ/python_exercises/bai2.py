e77cf7a322621fd0e14f046e4cb25ca870add883
Vũ Đức Long <herryvu.dev@gmail.com>
2025-04-24 21:11:55 +0700

    Update bai2.py

--- yendinh1/tim kiem nhi phan/LQDOJ/python_exercises/bai2.py
+++ yendinh1/tim kiem nhi phan/LQDOJ/python_exercises/bai2.py
@@ -1,7 +1,6 @@
 def tong(a, b):
     return a + b
 
-a = int(input("Nhập số thứ nhất: "))
-b = int(input("Nhập số thứ hai: "))
-kq = tong(a, b)
-print(f"Tổng của {a} và {b} là: {kq}")
+a = int(input())
+b = int(input())
+print(tong(a, b))
