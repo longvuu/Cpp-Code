bf378294476b3ed850ea34b19986842cdcda88b8
kuronight29 <herryvu.dev@gmail.com>
2025-05-15 07:55:48 +0700

    up

--- yendinh1/bua thang (bang thua)/A.cpp
+++ yendinh1/bua thang (bang thua)/A.cpp
@@ -9,38 +9,40 @@
 #define pb push_back
 using namespace std;
 const int N = 1e6 + 5;
-int a[N], a2[N];
-int n,q;
-void pre()
-{
-    for (int i = 1; i < n; i++)
-        a2[i] = min(a[i], a[i + 1]);
-}
-int xuly(int l, int r)
-{
-    int len = r - l + 1;
-    if (len == 1)
-        return a[l];
-    int mn = INT_MAX;
-    for (int i = l; i < r; i += 2)
-        mn = min(mn, a2[i]);
-    mn = min(mn, a2[r - 1]);
+const int LOG = 21;
+int a[N];
+int st[N][LOG];
+int logg[N];
+int n, q;
 
-    return mn;
+void pre() {
+    logg[1] = 0;
+    for (int i = 2; i < N; i++)
+        logg[i] = logg[i / 2] + 1;
+    for (int i = 1; i <= n; i++)
+        st[i][0] = a[i];
+    for (int j = 1; (1 << j) <= n; j++)
+        for (int i = 1; i + (1 << j) - 1 <= n; i++)
+            st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+}
+
+int xuly(int l, int r) {
+    int len = r - l + 1;
+    int k = logg[len];
+    return min(st[l][k], st[r - (1 << k) + 1][k]);
 }
 
 int main()
 {
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(0);cout.tie(0);
     if (fopen((string(taskname) + ".inp").c_str(), "r") != NULL)
     {
         freopen((string(taskname) + ".inp").c_str(), "r", stdin);
         freopen((string(taskname) + ".out").c_str(), "w", stdout);
     }
     cin >> n >> q;
-    for (int i = 1; i <= n;i++)
+    for (int i = 1; i <= n; i++)
         cin >> a[i];
     pre();
     while (q--)
--- yendinh1/bua thang (bang thua)/B.cpp
+++ yendinh1/bua thang (bang thua)/B.cpp
@@ -2,51 +2,62 @@
     @author longvuuuu
 */
 #include <bits/stdc++.h>
-#define taskname ""
-#define ll long long
-#define fi first
-#define se second
-#define pb push_back
 using namespace std;
-const int N = 1e6 + 5;
-int a[N], a2[N];
-int n,q;
-void pre()
-{
-    for (int i = 1; i < n; i++)
-        a2[i] = min(a[i], a[i + 1]);
-}
-int xuly(int l, int r)
-{
-    int len = r - l + 1;
-    if (len == 1)
-        return a[l];
-    int mn = INT_MAX;
-    for (int i = l; i < r; i += 2)
-        mn = min(mn, a2[i]);
-    mn = min(mn, a2[r - 1]);
+const int M = 505, N = 505;
+const int LGM = 10, LGN = 10;
+int a[M][N];
+int st[LGM][M][LGN][N];
+int n, m, q;
 
-    return mn;
+void pre() {
+    for (int k = 0; k <= LGM; k++) {
+        for (int i = 0; i + (1 << k) - 1 < n; i++) {
+            for (int l = 0; l <= LGN; l++) {
+                for (int j = 0; j + (1 << l) - 1 < m; j++) {
+                    if (k == 0) {
+                        if (l == 0) {
+                            st[0][i][0][j] = a[i][j];
+                        }
+                        else {
+                            st[0][i][l][j] = min(st[0][i][l - 1][j], st[0][i][l - 1][j + (1 << (l - 1))]);
+                        }
+                    }
+                    else {
+                        st[k][i][l][j] = min(st[k - 1][i][l][j], st[k - 1][i + (1 << (k - 1))][l][j]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+int xuly(int x1, int y1, int x2, int y2) {
+    int k = __lg(x2 - x1 + 1);
+    int l = __lg(y2 - y1 + 1);
+    return min({
+        st[k][x1][l][y1],
+        st[k][x1][l][y2 - (1 << l) + 1],
+        st[k][x2 - (1 << k) + 1][l][y1],
+        st[k][x2 - (1 << k) + 1][l][y2 - (1 << l) + 1]
+    });
 }
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    if (fopen((string(taskname) + ".inp").c_str(), "r") != NULL)
-    {
-        freopen((string(taskname) + ".inp").c_str(), "r", stdin);
-        freopen((string(taskname) + ".out").c_str(), "w", stdout);
-    }
-    cin >> n >> q;
-    for (int i = 1; i <= n;i++)
-        cin >> a[i];
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> a[i][j];
     pre();
+    cin >> q;
     while (q--)
     {
-        int l, r;
-        cin >> l >> r;
-        cout << xuly(l, r) << '\n';
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        x1--; y1--; x2--; y2--;
+        cout << xuly(x1, y1, x2, y2) << '\n';
     }
     return 0;
 }
